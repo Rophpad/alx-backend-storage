446625a43994f522fb1af4ddb23f932561c7f928
Rophpad <rophenp@gmaial.com>
2024-03-21 12:26:05 +0000

    remove file

--- 1-country_users.sql
+++
@@ -1,9 +0,0 @@
--- SQL script that creates a tables users following some requirements.
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country VARCHAR(2) NOT NULL DEFAULT 'US',
-    CONSTRAINT country_enum CHECK (country IN ('US', 'CO', 'TN'))
-);
--- 2-fans.sql
+++
@@ -1,3 +0,0 @@
--- SQL script that ordered
-
-SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
