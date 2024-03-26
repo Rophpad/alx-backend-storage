e343ed141126dd0117ee75c6fcfebadd76c22131
Rophpad <rophenp@gmail.com>
2024-03-26 15:42:12 +0100

    Python funct to update a doc in collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Python function that update a doc"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update many rows
+    """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
