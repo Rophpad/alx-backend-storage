6f46ab5957ec80b02c9d27ba4f2d283bdb002889
Rophpad <rophenp@gmail.com>
2024-03-27 15:37:57 +0100

    conformed code to pycodestyle

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,13 +4,13 @@ from typing import Union
 import redis
 import uuid
 
+
 class Cache():
     """Cache class"""
     def __init__(self):
         """Store an instance of the Redis client"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate random key"""
